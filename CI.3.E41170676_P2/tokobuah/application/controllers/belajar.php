c7e83bb6a1a78532917584ee6cee1d2e1ce55d46
AnggiTD <anggitrikusuma25@gmail.com>
2019-03-19 20:13:45 +0700

    view dan helper

--- CI.3.E41170676_P2/tokobuah/application/controllers/belajar.php
+++ CI.3.E41170676_P2/tokobuah/application/controllers/belajar.php
@@ -1,14 +1,17 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
  
-class Welcome extends CI_Controller {
+class Belajar extends CI_Controller {
 	
 	function __construct(){
 		parent::__construct();
 		$this->load->helper('html');
+		
 	}
  
-	public function index(){
-		$this->load->view('welcome');
+
+	public function index(){				
+		$this->load->view('view_belajar');
 	}
+ 
 }
