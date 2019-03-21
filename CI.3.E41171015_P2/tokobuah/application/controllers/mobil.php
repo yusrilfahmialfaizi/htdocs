7b6ee2aad6ffd1f7aabb937b5190e2d473a3e0f3
Yusril Fahmi Al Faizi <44188722+yusrilfahmialfaizi@users.noreply.github.com>
2019-03-21 15:18:26 +0700

    4-8

---
+++ CI.3.E41171015_P2/tokobuah/application/controllers/mobil.php
@@ -0,0 +1,21 @@
+<?php 
+	defined('BASEPATH') OR exit('No direct script access allowed');
+
+	/**
+	 * 
+	 */
+	class Mobil extends CI_Controller
+	{
+		
+		public function warna()
+		{
+			# code...
+			// echo "Mobil itu berwarna " . $this->uri->segment('3');
+			echo "Segment 1 adalah = " . $this->uri->segment('1') . "<br/>";		
+			echo "Segment 2 adalah = " . $this->uri->segment('2') . "<br/>";		
+			echo "Segment 3 adalah = " . $this->uri->segment('3') . "<br/>";		
+			echo "Segment 4 adalah = " . $this->uri->segment('4') . "<br/>";		
+			echo "Segment 5 adalah = " . $this->uri->segment('5') . "<br/>";	
+		}
+	}
+?>
---
+++ CI.3.E41171015_P2/tokobuah/application/libraries/malasngoding.php
@@ -0,0 +1,18 @@
+<?php 
+	/**
+	 * 
+	 */
+	class Malasngoding
+	{
+		
+		function nama_saya()
+		{
+			# code...
+			echo "Nama saya adalah Malasngoding";
+		}
+		function nama_kamu($nama)
+		{
+			echo "Nama kamu adalah " .$nama. "!";
+		}
+	}
+?>
